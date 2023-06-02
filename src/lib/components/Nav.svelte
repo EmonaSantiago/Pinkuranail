ba1ec819114f9f6b2e3d111c18b012496d803712
Emona Santiago <emonasantiago1207@gmail.com>
2023-06-02 21:23:58 +0200

    Nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,13 +1,51 @@
-<section id="navigatie">
-  <ul>
-    <li><a href="/Online-Boeken">Online Boeken</a></li>
-    <li><a href="/Over-ons">Over ons</a></li>
-  </ul>
-</section>
+<script>
+  import Image from "/Users/emonasantiago/Documents/choices-choices-the-tech-stack/Pinkuranail/src/images/logo.png";
+</script>
+
+<body>
+  <header class="nav-header">
+    <a href="/Online-Boeken" class="nav-link">Boeken</a>
+    <div>
+      <img src={Image} alt="My Image" />
+    </div>
+    <a href="/Over-ons" class="nav-link">Over ons</a>
+  </header>
+</body>
 
 <style>
-  #navigatie {
-    background-color: pink;
-    color: aqua;
+  .nav-header {
+    background-color: var(--pink);
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 300px;
+  }
+
+  img {
+    width: 10em;
+    height: auto;
+  }
+
+  .nav-links {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .nav-link {
+    text-decoration: none;
+    color: white;
+    margin: 0 10px;
+    font-weight: bold;
+  }
+
+  .nav-link:hover {
+    color: var(--red);
+  }
+
+  @media only screen and (max-width: 375px) {
+    .nav-header {
+      padding: 0 20px;
+    }
   }
 </style>
