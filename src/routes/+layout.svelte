2320189e622ceac9ebe57abfe48bd44b4c446ac2
Emona Santiago <emonasantiago1207@gmail.com>
2023-06-04 22:01:31 +0200

    fixed style route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
   import Nav from "$lib/components/Nav.svelte";
   import Footer from "$lib/components/Footer.svelte";
-  import "/Users/emonasantiago/Documents/choices-choices-the-tech-stack/Pinkuranail/src/css/style.css";
 </script>
 
 <Nav />
